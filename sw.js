17291c9298d943037b498f57c1d6404e10fc8918
sontyounomusuko-maker <274958356+sontyounomusuko-maker@users.noreply.github.com>
2026-09-24 20:26:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ドット漢方堂 オフライン用サービスワーカー
 // ゲームを更新したら VERSION の数字を1つ上げてください
-const VERSION = "v3";
+const VERSION = "v5";
 const CACHE = "dot-kampo-" + VERSION;
 const FILES = ["./","index.html","manifest.webmanifest","fonts.css","icons/icon-192.png","icons/icon-512.png","icons/icon-maskable-512.png","icons/apple-touch-icon.png","icons/favicon.png","fonts/dotgothic16-19-400-normal.woff2","fonts/dotgothic16-21-400-normal.woff2","fonts/dotgothic16-28-400-normal.woff2","fonts/dotgothic16-36-400-normal.woff2","fonts/dotgothic16-japanese-400-normal.woff2","fonts/dotgothic16-latin-400-normal.woff2"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
